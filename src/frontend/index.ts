eca16ef59877f6f52686298b3b03404bcf8fb706
Thomas-6666 <gamingteuteu@gmail.com>
2023-10-11 17:09:33 +0200

    Maj

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -19,16 +19,21 @@ const width = canvas.width
 const height = canvas.height
 const cellSize = Math.round(Math.min(width, height) / 9)
 const cellDomains: number[][][] = []
-const cellValues: (number | null)[][] = []
+const cellValues: (number | null)[][][] = []
 
 for (let j = 0; j < 9; j++) {
     cellDomains.push([])
     cellValues.push([])
     for (let i = 0; i < 9; i++) {
         cellDomains[j].push([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        cellValues[j].push(null)
+        cellValues[j].push(Array(9).fill(null))
     }
 }
+
+drawEmptyGrid()
+drawDomains()
+
+let selectedCell: [number, number] | null = null
 
 function clearCanvas() {
     ctx.fillStyle = bgColor
@@ -71,6 +76,7 @@ function drawEmptyGrid() {
 
 function drawDomain(i: number, j: number) {
     const domain = cellDomains[j][i]
+    const values = cellValues[j][i]
     const areaSize = Math.max(cellSize - 2, Math.floor(cellSize * 0.2))
     const valueStep = Math.floor(areaSize / 3)
     const cellPadding = Math.max(1, Math.floor(cellSize * 0.1))
@@ -78,7 +84,7 @@ function drawDomain(i: number, j: number) {
     const y = j * cellSize + cellPadding
 
     for (let k = 1; k <= 9; k++) {
-        const vk = domain.includes(k) ? k : null
+        const vk = domain.includes(k) && !values.includes(k) ? k : null
         const vi = (k - 1) % 3
         const vj = Math.floor((k - 1) / 3)
         const vx = x + valueStep * vi
@@ -100,24 +106,15 @@ function drawDomains() {
     }
 }
 
-drawEmptyGrid()
-drawDomains()
-
-
-let selectedCell: [number, number] | null = null
-
 canvas.addEventListener("mousemove", function (event: MouseEvent) {
     event.stopPropagation()
     const x = event.offsetX
     const y = event.offsetY
     const i = Math.min(Math.floor(x / cellSize), 9)
     const j = Math.min(Math.floor(y / cellSize), 9)
-    if (selectedCell === null || selectedCell[0] !== i || selectedCell[2] !== j) {
+    if (selectedCell === null || selectedCell[0] !== i || selectedCell[1] !== j) {
         selectedCell = [i, j]
         console.log("Celulle selectionnée : ", selectedCell)
-        document.addEventListener("toggle", function (event:ToggleEvent){
-            console.log(event)
-        })
     }
 })
 
@@ -125,5 +122,40 @@ canvas.addEventListener("mouseout", function (event: MouseEvent) {
     selectedCell = null
     console.log("Celulle selectionnée : ", selectedCell)
 })
+
+document.addEventListener("keyup", function (event: KeyboardEvent) {
+    let value = parseInt(event.key)
+    if (Number.isInteger(value) && selectedCell !== null) {
+        toogle(value)
+    }
+})
+
+function toogle(v: number) {
+    if (selectedCell) {
+        let i = selectedCell[0]
+        let j = selectedCell[1]
+        let pos = cellDomains[j][i].indexOf(v)
+        let x = i - (i % 3)
+        let y = j - (j % 3)
+
+        if (cellValues[j][i][pos] === null) {
+            //TODO
+            for (let n = 0; n < 3; n++) {
+                cellValues[j][x + n][pos] = null
+                cellValues[y + n][i][pos] = null
+            }
+            cellValues[j][i][pos] = cellDomains[j][i][pos]
+        } else {
+            for (let n = 0; n < 3; n++) {
+                cellValues[j][x + n][pos] = cellDomains[j][x + n][pos]
+                cellValues[y + n][i][pos] = cellDomains[y + n][i][pos]
+            }
+            cellValues[j][i][pos] = null
+        }
+
+        drawEmptyGrid()
+        drawDomains()
+    }
+}
 
 console.log("Frontend chargé")
