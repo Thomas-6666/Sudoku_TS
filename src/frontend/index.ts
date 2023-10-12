ae8c0e63a69d29d36a88e2ea30fd5a20743db431
Thomas-6666 <72489077+Thomas-6666@users.noreply.github.com>
2023-10-12 16:15:48 +0200

    Mise a jour

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,160 +1,138 @@
-/* /// <reference lib="dom"> /> */
+/* /// <reference lib="dom" /> */
 
-const ws = new WebSocket(`ws://${location.host}`);
-ws.onopen = () => setInterval(() => ws.send("ping"), 5000);
-ws.onmessage = (event: MessageEvent) => {
-    console.log(event.data);
-    if (event.data === "reload") {
-        location.reload()
-    }
+import { wsInit, SudokuUI, eventHandlersInit } from "./io"
+
+type InitialState = {
+	readonly canvas: HTMLCanvasElement
+	readonly ui: SudokuUI
+	readonly cellDomains: number[][][]
+	readonly cellValues: (number | null)[][]
 }
 
-const bgColor = "#CCC"
-const thinLineColor = "#777"
-const boldLineColor = "#000"
-
-const canvas = document.getElementById("sudokuCanvas") as HTMLCanvasElement
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
-const width = canvas.width
-const height = canvas.height
-const cellSize = Math.round(Math.min(width, height) / 9)
-const cellDomains: number[][][] = []
-const cellValues: (number | null)[][][] = []
-
-for (let j = 0; j < 9; j++) {
-    cellDomains.push([])
-    cellValues.push([])
-    for (let i = 0; i < 9; i++) {
-        cellDomains[j].push([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        cellValues[j].push(Array(9).fill(null))
-    }
+function init(canvasId: string): InitialState | false {
+	const canvas = document.getElementById(canvasId) as HTMLCanvasElement | null
+	if (canvas === null) {
+		console.error("Cannot get the given Canvas 2D rendering context")
+		return false
+	}
+	const ui = SudokuUI.get(canvas)
+	if (!ui) {
+		return false
+	}
+	const cellDomains: number[][][] = []
+	const cellValues: (number | null)[][] = []
+	for (let j = 0; j < 9; j++) {
+		cellDomains.push([])
+		cellValues.push([])
+		for (let i = 0; i < 9; i++) {
+			cellDomains[j].push([1, 2, 3, 4, 5, 6, 7, 8, 9])
+			cellValues[j].push(null)
+		}
+	}
+	return { canvas, ui, cellDomains, cellValues }
 }
 
-drawEmptyGrid()
-drawDomains()
+function start(initialState: InitialState) {
+	const {canvas, ui, cellDomains, cellValues} = initialState
+	let selectedCell: [number, number] | null = null
 
-let selectedCell: [number, number] | null = null
+	function drawCellContent(i: number, j: number) {
+		if (cellValues[j][i] !== null) {
+			ui.drawCellValue(i, j, cellValues[j][i]!)
+		} else {
+			ui.drawCellDomain(i, j, cellDomains[j][i])
+		}
+	}
 
-function clearCanvas() {
-    ctx.fillStyle = bgColor
-    ctx.fillRect(0, 0, width, height)
+	function drawCellsContent() {
+		for (let j = 0; j < 9; j++) {
+			for (let i = 0; i < 9; i++) {
+				drawCellContent(i, j)
+			}
+		}
+	}
+
+	function removeValueFromCellDomain(i: number, j: number, v: number) {
+		const domain = cellDomains[j][i]
+		const valueIndex = domain.indexOf(v)
+		if (valueIndex !== -1) {
+			domain.splice(valueIndex, 1)
+		}
+	}
+
+	function addValueToCellDomain(i: number, j: number, v: number) {
+		const domain = cellDomains[j][i]
+		if (!domain.includes(v)) {
+			domain.push(v)
+		}
+	}
+
+	function maintainImpactedCellsDomain(
+		i: number,
+		j: number,
+		v: number,
+		remove: boolean
+	) {
+		const action = remove ? removeValueFromCellDomain : addValueToCellDomain
+		for (let k = 0; k < 9; k++) {
+			if (k !== i) {
+				action(k, j, v)
+			}
+			if (k !== j) {
+				action(i, k, v)
+			}
+		}
+		const iGroup = Math.floor(i / 3)
+		const jGroup = Math.floor(j / 3)
+		for (let j2 = 0; j2 < 3 ; j2++) {
+			for (let i2 = 0; i2 < 3; i2++) {
+				const iCell = iGroup * 3 + i2
+				const jCell = jGroup * 3 + j2
+				if (iCell !== i && jCell !== j) {
+					action(iCell, jCell, v)
+				}
+			}
+		}
+	}
+
+	function toggle(v: number) {
+		const i = selectedCell![0]
+		const j = selectedCell![1]
+		if (cellValues[j][i] === null) {
+			if (cellDomains[j][i].includes(v)) {
+				cellValues[j][i] = v
+				maintainImpactedCellsDomain(i, j, v, true)
+				refreshGrid()
+			}
+		} else if (cellValues[j][i] === v) {
+			cellValues[j][i] = null
+			maintainImpactedCellsDomain(i, j, v, false)
+			for (let j2 = 0; j2 < 9; j2++) {
+				for (let i2 = 0; i2 < 9; i2++) {
+					if (cellValues[j2][i2] === v) {
+						maintainImpactedCellsDomain(i2, j2, v, true)
+					}
+				}
+			}
+			refreshGrid()
+		}
+	}
+
+	function refreshGrid() {
+		ui.drawEmptyGrid()
+			.colorizeSelectedStuff(selectedCell)
+		drawCellsContent()
+	}
+
+	eventHandlersInit({	canvas,	ui,	refreshGrid, toggle,
+		getSelectedCell: () => selectedCell,
+		setSelectedCell: (newCell: [number, number] | null) => selectedCell = newCell,
+	})
+	wsInit()
+	refreshGrid()
 }
 
-function drawCell(i: number, j: number, cellSize: number, borderColor: string, fillColor?: string) {
-    const x = i * cellSize
-    const y = j * cellSize
-    if (fillColor) {
-        ctx.fillStyle = fillColor
-        ctx.fillRect(x + 1, y + 1, cellSize - 2, cellSize - 2)
-    }
-    ctx.strokeStyle = borderColor
-    ctx.strokeRect(x, y, cellSize, cellSize)
+const retInit = init("sudokuCanvas")
+if (retInit) {
+	start(retInit)
 }
-
-function drawGroup(groupI: number, groupJ: number, fillColor?: string) {
-    drawCell(groupI, groupJ, cellSize * 3, boldLineColor, fillColor)
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            drawCell(groupI * 3 + i, groupJ * 3 + j, cellSize, thinLineColor)
-        }
-    }
-}
-
-function drawEmptyGrid() {
-    clearCanvas()
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            drawCell(i, j, cellSize, thinLineColor, bgColor)
-        }
-    }
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            drawGroup(i, j)
-        }
-    }
-}
-
-function drawDomain(i: number, j: number) {
-    const domain = cellDomains[j][i]
-    const values = cellValues[j][i]
-    const areaSize = Math.max(cellSize - 2, Math.floor(cellSize * 0.2))
-    const valueStep = Math.floor(areaSize / 3)
-    const cellPadding = Math.max(1, Math.floor(cellSize * 0.1))
-    const x = i * cellSize + cellPadding
-    const y = j * cellSize + cellPadding
-
-    for (let k = 1; k <= 9; k++) {
-        const vk = domain.includes(k) && !values.includes(k) ? k : null //Si k est également dans l'autre liste on n'affiche pas le chiffre
-        const vi = (k - 1) % 3
-        const vj = Math.floor((k - 1) / 3)
-        const vx = x + valueStep * vi
-        const vy = y + valueStep * vj
-        ctx.fillText(vk !== null ? vk.toString() : "", vx, vy)
-    }
-}
-
-function drawDomains() {
-    ctx.fillStyle = "#000"
-    ctx.font = "16px Arial"
-    ctx.textBaseline = "top"
-    ctx.textAlign = "start"
-
-    for (let j = 0; j < 9; j++) {
-        for (let i = 0; i < 9; i++) {
-            drawDomain(i, j)
-        }
-    }
-}
-
-canvas.addEventListener("mousemove", function (event: MouseEvent) {
-    event.stopPropagation()
-    const x = event.offsetX
-    const y = event.offsetY
-    const i = Math.min(Math.floor(x / cellSize), 9)
-    const j = Math.min(Math.floor(y / cellSize), 9)
-    if (selectedCell === null || selectedCell[0] !== i || selectedCell[1] !== j) {
-        selectedCell = [i, j]
-        console.log("Celulle selectionnée : ", selectedCell)
-    }
-})
-
-canvas.addEventListener("mouseout", function (event: MouseEvent) {
-    selectedCell = null
-    console.log("Celulle selectionnée : ", selectedCell)
-})
-
-document.addEventListener("keyup", function (event: KeyboardEvent) {
-    let value = parseInt(event.key)
-    if (Number.isInteger(value) && selectedCell !== null) {
-        toogle(value)
-    }
-})
-
-function toogle(v: number) {
-    if (selectedCell) {
-        let i = selectedCell[0]
-        let j = selectedCell[1]
-        let pos = cellDomains[j][i].indexOf(v) //Récupère la position du chiffre tapé dans la case selectionnée
-        let x = i - (i % 3)
-        let y = j - (j % 3)
-
-        if (cellValues[j][i][pos] === null) { //Desaffecte v d'une case
-            for (let n = 0; n < 3; n++) {
-                cellValues[j][x + n][pos] = null
-                cellValues[y + n][i][pos] = null
-            }
-            cellValues[j][i][pos] = cellDomains[j][i][pos]
-        } else { //Affecte v une case
-            for (let n = 0; n < 3; n++) {
-                cellValues[j][x + n][pos] = cellDomains[j][x + n][pos]
-                cellValues[y + n][i][pos] = cellDomains[y + n][i][pos]
-            }
-            cellValues[j][i][pos] = null
-        }
-
-        drawEmptyGrid()
-        drawDomains()
-    }
-}
-
-console.log("Frontend chargé")
